970a8069f21a094c209d0bf92d661bc37018b259
JulieRiise <143400005+JulieRiise@users.noreply.github.com>
2025-03-17 13:07:49 +0100

    pusher endringer

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import { Route, Routes } from "react-router-dom";
+import Resources from './components/Resources';
+import Layout from './components/Layout';
+
+
+
+function App() {
+ 
+  return (
+    <>
+      <Layout>
+        <Routes>
+            <Route path="/" element={<Resources category={"html"}/>}/>
+            <Route path="/html" element={<Resources category={"html"}/>}/>
+            <Route path="/css" element={<Resources category={"css"}/>}/>
+            <Route path="/javascript" element={<Resources category={"javascript"}/>}/>
+            <Route path="/react" element={<Resources category={"react"}/>}/>
+            <Route path="/headless-cms" element={<Resources category={"headless-cms"}/>}/>
+        </Routes>
+      </Layout>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/PageTitle.jsx
@@ -0,0 +1,3 @@
+export default function PageTitle({titilecategory}){
+    return <h1>{titilecategory}</h1>
+}
